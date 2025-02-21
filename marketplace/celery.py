372c4aaeead472fa4b24b5fe4622683d083108eb
Fronik13122 <asren67382@gmail.com>
2025-02-21 17:44:11 +0300

    исправлены ошибки с доступом и созданием заказов

--- marketplace/celery.py
+++ marketplace/celery.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
@@ -6,7 +7,12 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marketplace.settings')
 
 celery_app = Celery('marketplace')
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Добавляем настройку для устранения предупреждения
+celery_app.conf.broker_connection_retry_on_startup = True
+
 celery_app.autodiscover_tasks()
+
 
 @celery_app.task(bind=True)
 def debug_task(self):
