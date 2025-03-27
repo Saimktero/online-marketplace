9743954b63ac2380b54a309162926ca08ae3dce4
Fronik13122 <asren67382@gmail.com>
2025-03-28 00:07:01 +0300

    Удалён .env и старые настройки из Git, добавлен .env в .gitignore

---
+++ config/celery.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+# Указываем Django settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+celery_app = Celery('marketplace')
+celery_app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Добавляем настройку для устранения предупреждения
+celery_app.conf.broker_connection_retry_on_startup = True
+
+celery_app.autodiscover_tasks()
+
+
+@celery_app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
